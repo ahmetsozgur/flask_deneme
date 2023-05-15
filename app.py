dfd839144756496e191d6c8a84219b93c8ee2189
ahmetsozgur <133383388+ahmetsozgur@users.noreply.github.com>
2023-05-15 17:04:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,24 @@
 from flask import Flask, request, render_template
+import numpy as np    
+from numpy import nan
+import os 
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error,mean_absolute_error,mean_absolute_percentage_error,r2_score
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, ensemble
+from sklearn.inspection import permutation_importance
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import f1_score, precision_score,recall_score,cohen_kappa_score,roc_curve,auc 
+from sklearn.model_selection import train_test_split
+from flask import Flask, request, render_template
  
 # Flask constructor
 app = Flask(__name__)  
